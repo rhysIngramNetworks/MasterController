2558c50132247775050c9b2a1124f47436379d76
Rhys Hopkins <rhys@ingramnetworks.net>
2025-08-07 16:22:40 +0100

    tested modem disable dc offset loop

--- modem/modem_controller.py
+++ modem/modem_controller.py
@@ -70,13 +70,9 @@ class ModemController:
         self.telnet.read_until(b"x. Exit")
         self.telnet.write(b"3\n")
 
-        #Modules
+        #FPGA Modules
         self.telnet.read_until(b"x. Exit")
         self.telnet.write(b"1\n")
-
-        #TX Modules
-        self.telnet.read_until(b"x. Exit")
-        self.telnet.write(b"2\n")
 
         #Digital Tx
         self.telnet.read_until(b"x. Exit")
@@ -129,103 +125,3 @@ class ModemController:
         time.sleep(0.5)
         # Close the Telnet session when done
         self.telnet.close()
-
-    
-"""
-import telnetlib
-import sys
-import time
-
-# Define your Telnet connection parameters
-host = "192.168.128.21"
-user1 = "ssh"
-password1 = "ssh"
-user2 = "admin"
-password2 = "admin"
-
-
-# Create a Telnet object and connect to the device
-tn = telnetlib.Telnet(host)
-print("start")
-
-# Read the first username prompt
-tn.read_until(b"login:")
-print("done")
-
-tn.write(user1.encode("utf-8") + b"\n")
-print("done")
-
-# Read the first password prompt and send the first password
-tn.read_until(b"Password:")
-print("done")
-
-tn.write(password1.encode("utf-8") + b"\n")
-print("done")
-
-# Read the second username prompt
-tn.read_until(b"Username:")
-print("done")
-
-tn.write(user2.encode("utf-8") + b"\n")
-print("done")
-
-# Read the second password prompt and send the second password
-tn.read_until(b"Password:")
-print("done")
-
-tn.write(password2.encode("utf-8") + b"\n")
-print("done")
-
-#Main Menu
-tn.read_until(b"x. Exit")
-print("#Main Menu")
-
-tn.write(b"3\n")
-print("done")
-
-#Modules
-tn.read_until(b"x. Exit")
-print("#Modules")
-
-tn.write(b"1\n")
-print("done")
-
-#TX Modules
-tn.read_until(b"x. Exit")
-print("#TX Modules")
-
-tn.write(b"2\n")
-print("done")
-
-#AcmController
-tn.read_until(b"x. Exit")
-print("#AcmController")
-
-tn.write(b"1\n")
-print("done")
-
-#Enter Profile Manual Value
-tn.read_until(b"(y/n x=exit)")
-print("#Enter Profile Manual Value")
-
-tn.write("y\n".encode("utf-8"))
-print("done")
-
-#Set Profile Manual Value
-tn.read_until(b"('x' to Exit):")
-
-print("#Set Profile Manual Value")
-
-#tn.write(b"Mod_Rate")
-ModRate = sys.argv[1]
-Mod_Rate = ModRate
-mod_rate = str(Mod_Rate)
-tn.write(mod_rate.encode("utf-8") + b"\n")
-print("done")
-print(mod_rate, Mod_Rate)
-
-time.sleep(0.5)
-# Close the Telnet session when done
-tn.close()
-print("closed")
-"""
--- scripts/modem_setup.py
+++ scripts/modem_setup.py
@@ -1,25 +1,13 @@
 import sys
-import time
-
+ 
+sys.path.append("C:/Users/RhysHopkins/Documents/repositories/MasterController")
 sys.path.append("/home/trains/Documents/repositories/MasterController/")
-sys.path.append("C:/Users/RhysHopkins/Documents/repositories/MasterController/")
 
 from modem.modem_controller import ModemController
 
 
-modem_controller = ModemController("192.168.128.11",
-                                   "ssh",
-                                   "ssh",
-                                   "admin",
-                                   "admin")
+modem = ModemController("192.168.128.11", "ssh", "ssh", "admin", "admin")
 
-modem_controller.login()
-modem_controller.enable(0)
-
-time.sleep(1)
-
-modem_controller.enable(1)
-
-time.sleep(1)
-
-modem_controller.enable(0)
+modem.set_dc_offset()
+modem.logout()
+modem.close()
